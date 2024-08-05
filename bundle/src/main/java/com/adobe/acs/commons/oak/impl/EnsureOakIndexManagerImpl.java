82667d84d40bcbc74d6c0a1dd772f19527ab5ad9
Jasper Simon <jasper.simon2@kbc.be>
2024-08-05 13:01:29 +0200

    Make sure creations or updates of EnsureOakIndex configurations get picked up

--- bundle/src/main/java/com/adobe/acs/commons/oak/impl/EnsureOakIndexManagerImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/oak/impl/EnsureOakIndexManagerImpl.java
@@ -21,14 +21,7 @@ import com.adobe.acs.commons.oak.EnsureOakIndexManager;
 import com.adobe.acs.commons.util.RequireAem;
 import com.adobe.granite.jmx.annotation.AnnotatedStandardMBean;
 import org.apache.commons.lang.StringUtils;
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.ReferenceCardinality;
-import org.apache.felix.scr.annotations.ReferencePolicy;
-import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.*;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -111,6 +104,7 @@ public class EnsureOakIndexManagerImpl extends AnnotatedStandardMBean implements
 
     @Reference(
         cardinality = ReferenceCardinality.OPTIONAL_MULTIPLE,
+        policyOption = ReferencePolicyOption.GREEDY,
         referenceInterface = AppliableEnsureOakIndex.class,
         policy = ReferencePolicy.DYNAMIC
     )
